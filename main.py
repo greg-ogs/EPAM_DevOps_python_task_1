e880741b474d55a1b0cccc06ad86cf194439aeeb
Alex <65621681+greg-ogs@users.noreply.github.com>
2024-11-28 00:22:15 -0600

    Wheel (#1)
    * First:Packege created.
    Second:Main for test
    Third:Wheel packege done
    Fourth: Dockerfile updated and requirements.txt too.
    
    * First: Dockerfile entry point fixed
    Second: Docker image ready
    Third: system exit fix in main.py

--- main.py
+++ main.py
@@ -3,36 +3,33 @@ Created by greg-ogs for EPAM DevOps specialization.
 Nov 2024
 '''
 
-import sys
-
 # Example of the first part
-
-from Classes import Dictionary, Tiket, WordConcatenator
+import sys
+from pytask.Classes import Dictionary, Tiket, WordConcatenator
 
 if __name__ == '__main__':
     # For part 1
-    # d = Dictionary()
-    # d.add("Banana", "A yellow fruit")
-    # print(d.lookup("Banana"))  # Output: A yellow fruit
-    # print(d.lookup("Apple"))  # Output: Word not found in the dictionary.
-    # sys.exit(0)
+    d = Dictionary()
+    d.add("Banana", "A yellow fruit")
+    print(d.lookup("Banana"))  # Output: A yellow fruit
+    print(d.lookup("Apple"))  # Output: Word not found in the dictionary.
 
     # For part 2
-    # items_and_costs = {
-    #     "apple": 1.00,
-    #     "banana": 0.50,
-    #     "orange": 1.25
-    # }
-    # receipt = Tiket(items_and_costs)
-    #
-    # # Get items bought from user input
-    # items_str = input("Enter the items bought, separated by commas: ")
-    # items_bought = [item.strip() for item in items_str.split(",")]
-    # # items_bought = ["apple", "banana", "grapefruit"]  # Grapefruit is not in the dictionary
-    # tax_rate = 0.1  # 10% tax
-    #
-    # total_cost = receipt.calculate_total(items_bought, tax_rate)
-    # print(f"Total cost: ${total_cost:.2f}")
+    items_and_costs = {
+        "apple": 1.00,
+        "banana": 0.50,
+        "orange": 1.25
+    }
+    receipt = Tiket(items_and_costs)
+
+    # Get items bought from user input
+    items_str = input("Enter the items bought, separated by commas: ")
+    items_bought = [item.strip() for item in items_str.split(",")]
+    # items_bought = ["apple", "banana", "grapefruit"]  # Grapefruit is not in the dictionary
+    tax_rate = 0.1  # 10% tax
+
+    total_cost = receipt.calculate_total(items_bought, tax_rate)
+    print(f"Total cost: ${total_cost:.2f}")
 
     #For part 3
     words_str = input("Enter the words, separated by commas: ")
@@ -40,4 +37,5 @@ if __name__ == '__main__':
     # words = ["soda", "best", "has", "net", "hello", "yes", "computer"]
     concatenator = WordConcatenator(words)
     result = concatenator.concatenate()
-    print(result)  # Output: yes
+    print(result)
+    sys.exit(0)
